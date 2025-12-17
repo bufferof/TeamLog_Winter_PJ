15b974520b05c2403a94c1179a0ae5af2292e257
bufferof <158188942+bufferof@users.noreply.github.com>
2025-12-17 11:47:17 +0900

    HTML & JS 수정

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded',()=>{
         start_btn.disable = true //중복 클릭 방지
 
         //데이터 가져오기
-        const send_text = document.getElementById('')
+        const send_text = document.getElementById('text_send').value;
+        const target_id = document.getElementById('target').value;       
+
+        
     });
 });
